717b5fffbdb77aeff7d43e524520278925ebefbf
Senhil <senthilptj@gmail.com>
2022-01-08 21:45:51 +0530

    Day 16-17 (Data Structure and Algorithm Problems)

---
+++ src/AssignmentDay16and17/Node.java
@@ -0,0 +1,14 @@
+package AssignmentDay16and17;
+
+class Node {
+
+    public Node(Object data) {
+        super();
+        this.data = data;
+        this.next = null;
+    }
+
+
+    Object data;
+    Node next;
+}
---
+++ src/AssignmentDay16and17/PrimeNumber.java
@@ -0,0 +1,11 @@
+package AssignmentDay16and17;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        Utility u = new Utility();
+        System.out.println("Enter the number:");
+        int x = u.inputInteger();
+        System.out.println(u.primeNumber(x));
+    }
+}
